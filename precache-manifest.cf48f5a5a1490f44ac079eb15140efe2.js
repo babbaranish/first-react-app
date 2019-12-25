5a13310fe3c1c14a37b303d0677b8dd3fe47020e
babbaranish <anishbabbar1@gmail.com>
2019-12-25 19:48:32 +0530

    Updates

---
+++ precache-manifest.cf48f5a5a1490f44ac079eb15140efe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb31bcc7ad993c973bcbbc12356d819",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "c52b4566ccf9e50be7da",
+    "url": "/first-react-app/static/css/main.b31908f5.chunk.css"
+  },
+  {
+    "revision": "2b732637eab9567ef272",
+    "url": "/first-react-app/static/js/2.2c51c45c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/first-react-app/static/js/2.2c51c45c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c52b4566ccf9e50be7da",
+    "url": "/first-react-app/static/js/main.210d149a.chunk.js"
+  },
+  {
+    "revision": "6e92a322ff8628a2945b",
+    "url": "/first-react-app/static/js/runtime-main.5cf03c4e.js"
+  }
+]);
